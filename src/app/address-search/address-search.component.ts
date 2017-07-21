9d35e3ea2e4a493780efa26dac5ea8edb731f7c9
Jared Luedtke <jared7223@hotmail.com>
2017-07-21 10:51:01 -0700

    implement search by address api call

--- src/app/address-search/address-search.component.ts
+++ src/app/address-search/address-search.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { GeocodeService } from '../geocode.service';
 
 @Component({
   selector: 'app-address-search',
   templateUrl: './address-search.component.html',
-  styleUrls: ['./address-search.component.css']
+  styleUrls: ['./address-search.component.css'],
+  providers: [GeocodeService]
 })
 export class AddressSearchComponent implements OnInit {
+  coordinateInfo: any[];
 
-  constructor() { }
+  constructor(private geoService: GeocodeService) { }
 
   ngOnInit() {
+  }
+
+  addressInput(address: string) {
+    let formattedAddress: string = address.replace(/( )/, '+');
+    console.log(formattedAddress);
+    this.geoService.getCoordinatesFromAddress(formattedAddress).subscribe(data => {
+      if (data.json().results.length > 0) {
+        this.coordinateInfo = data.json();
+        console.log(this.coordinateInfo);
+      } else {
+        console.log('No Data');
+      }
+    });
   }
 
 }
